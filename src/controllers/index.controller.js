e46680a5be14257bda8121b4b97e07e5d7314578
Hector David Cabrera <53708622+hdavidvc@users.noreply.github.com>
2022-10-30 00:49:51 -0400

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 
 const getBook = async(req, res) => {
     const response = await pool.query('select * from books');
-    res.status(200).json({ Titulo:"variable", content:response.rows});
+    res.status(200).json({ Titulo:"llll", content:response.rows});
     //res.send(response.rows);
     console.log(response.rows);
     // pool.end(); 
